118150abdc99745f99002b373c6c60a0ebc059b0
Brariv <119123309+Brariv@users.noreply.github.com>
2024-03-02 11:37:04 -0600

    Code Finished

--- HDT5.py
+++ HDT5.py
@@ -1,30 +1,39 @@
 import simpy
 import random
+import time
+
 
 # Constants
+CSV_FILE = 'Process_time_Start.csv'
 NUM_PROCESS = 25
+Inter_process = 3
 CPU_SIZE = 1
 RAM_SIZE = 100
-GAS_STATION_WAIT_MEAN = 5
+Num_CPU = 1
+Interval = 10
+
+random.seed(10)
+Process_time = {}
 
 
 # Function to simulate process solving
-def process(env, proc_id, laps, CPU, RAM, RAM_SIZE):
-    print(f'{env.now}:{proc_id} llega a la cola del waiting room. Procesos en espera {len(CPU.queue)}')
-    while (laps > 0):
+def process(env, proc_id, instructions, CPU, RAM, RAM_usage,Start):
+    
+    print(f'{env.now}: {proc_id} llega a la cola del waiting room. Procesos en espera {len(CPU.queue)}')
+    while (instructions > 0):
         with CPU.request() as req:
             yield req
             print(f'{env.now}: {proc_id} Está listo para entrar a proceso.')
-            if laps == 0:
+            if instructions == 0:
                 break
-            elif laps < 3:
-                laps_cant = laps
+            elif instructions < Inter_process:
+                instructions_cant = instructions
             else:
-                laps_cant = 3
+                instructions_cant = Inter_process
             
-            for lap in range(1, laps_cant + 1):
-                print(f'{env.now}: {proc_id} instrucciones restantes: {laps}')
-                laps -= 1
+            for lap in range(1, instructions_cant + 1):
+                print(f'{env.now}: {proc_id} instrucciones restantes: {instructions}')
+                instructions -= 1
                 yield env.timeout(1)  # Time to complete Proces
             
         option = random.randint(1, 2)
@@ -35,21 +44,33 @@ def process(env, proc_id, laps, CPU, RAM, RAM_SIZE):
 
         
 
-##            if lap % 3 == 0:  # Time for gas after every 3 laps
-        RAM.put(RAM_SIZE)
-        print(f' {proc_id} exits the cpu at {env.now}')
+# End of the process
+    
+    RAM.put(RAM_usage)
+    print(f'{env.now}: {proc_id} exits the cpu at {env.now}')
+    End = time.perf_counter()
+    timet = f"{End - Start:0.4f}"
+    Process_time[proc_id] = timet
+    
 
 
 # Function to generate instructions with ram capacity
 def generate_process(env, CPU, RAM):
+    intval = round(random.expovariate(1.0 / Interval))
+    
     RAM_usage = random.randint(1, 10)
-    with RAM.get(RAM_usage) as req:
-        yield req
-        for i in range(NUM_PROCESS):
-            laps = random.randint(1, 10)
+    for i in range(NUM_PROCESS):
+        yield env.timeout(intval)
+        Start = time.perf_counter()
+        print(f'{env.now}: Proc-{i+1} espera a suficiente memoria RAM: {RAM.level}/{RAM_SIZE}')
+        with RAM.get(RAM_usage) as reqRAM:
+            yield reqRAM
+            print(f'{env.now}: Proc-{i+1} tiene suficiente memoria RAM')
+            instructions = random.randint(1, 10)
             proc_id = f'Proc-{i+1}'
-            env.process(process(env, proc_id, laps, CPU, RAM, RAM_SIZE))
-            yield env.timeout(2)  # Time between car arrivals
+            
+            env.process(process(env, proc_id, instructions, CPU, RAM, RAM_usage,Start))
+
 
 
 # Setup and start the simulation
@@ -58,3 +79,8 @@ RAM = simpy.Container(env, init=RAM_SIZE, capacity=RAM_SIZE)
 CPU = simpy.Resource(env, capacity=CPU_SIZE)
 env.process(generate_process(env, CPU, RAM))
 env.run()
+
+with open(CSV_FILE, 'w') as file:
+    file.write('Process, Time\n')
+    for key, value in Process_time.items():
+        file.write(f'{key}, {value}\n')
