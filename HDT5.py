b212d230c040cd168c89fd84e815a0ec87aa0b8e
Brariv <119123309+Brariv@users.noreply.github.com>
2024-03-02 12:56:22 -0600

    CSV, Graficas y promedios

--- HDT5.py
+++ HDT5.py
@@ -3,21 +3,22 @@ import random
 import time
 
 
+
 # Constants
-CSV_FILE = 'Process_time_Start.csv'
-NUM_PROCESS = 25
+CSV_FILE = 'Process_time_I1.csv'
+
 Inter_process = 3
 CPU_SIZE = 1
 RAM_SIZE = 100
-Num_CPU = 1
-Interval = 10
-
+Interval = 1
+NUM_PROCESS = 200
+TimeAv = []
 random.seed(10)
-Process_time = {}
+Process_time = []
 
 
 # Function to simulate process solving
-def process(env, proc_id, instructions, CPU, RAM, RAM_usage,Start):
+def process(env, proc_id, instructions, CPU, RAM, RAM_usage,Start,NUM_PROCESS):
     
     print(f'{env.now}: {proc_id} llega a la cola del waiting room. Procesos en espera {len(CPU.queue)}')
     while (instructions > 0):
@@ -50,18 +51,17 @@ def process(env, proc_id, instructions, CPU, RAM, RAM_usage,Start):
     print(f'{env.now}: {proc_id} exits the cpu at {env.now}')
     End = time.perf_counter()
     timet = f"{End - Start:0.4f}"
-    Process_time[proc_id] = timet
+    TimeAv.append(float(timet))
     
 
 
 # Function to generate instructions with ram capacity
-def generate_process(env, CPU, RAM):
+def generate_process(env, CPU, RAM, NUM_PROCESS):
+    Start = time.perf_counter()
     intval = round(random.expovariate(1.0 / Interval))
-    
     RAM_usage = random.randint(1, 10)
     for i in range(NUM_PROCESS):
         yield env.timeout(intval)
-        Start = time.perf_counter()
         print(f'{env.now}: Proc-{i+1} espera a suficiente memoria RAM: {RAM.level}/{RAM_SIZE}')
         with RAM.get(RAM_usage) as reqRAM:
             yield reqRAM
@@ -69,7 +69,7 @@ def generate_process(env, CPU, RAM):
             instructions = random.randint(1, 10)
             proc_id = f'Proc-{i+1}'
             
-            env.process(process(env, proc_id, instructions, CPU, RAM, RAM_usage,Start))
+            env.process(process(env, proc_id, instructions, CPU, RAM, RAM_usage,Start,NUM_PROCESS))
 
 
 
@@ -77,10 +77,15 @@ def generate_process(env, CPU, RAM):
 env = simpy.Environment()
 RAM = simpy.Container(env, init=RAM_SIZE, capacity=RAM_SIZE)
 CPU = simpy.Resource(env, capacity=CPU_SIZE)
-env.process(generate_process(env, CPU, RAM))
+env.process(generate_process(env, CPU, RAM, NUM_PROCESS))
 env.run()
 
-with open(CSV_FILE, 'w') as file:
-    file.write('Process, Time\n')
-    for key, value in Process_time.items():
-        file.write(f'{key}, {value}\n')
+# Print the average time of the process
+print(f'Average time: {sum(TimeAv) / len(TimeAv)}')
+Process_time.append(NUM_PROCESS)
+Process_time.append(sum(TimeAv) / len(TimeAv))
+
+# Append the average time of the process to the CSV file
+with open(CSV_FILE, 'a') as file:
+    file.write(f'{Process_time[0]},{Process_time[1]}\n')
+    file.close()
